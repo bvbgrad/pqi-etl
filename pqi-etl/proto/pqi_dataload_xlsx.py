d66c2fa802097cff25405498651f6630f0eaa6b5
Brent V. Bingham <bvbgrad@gmail.com>
2022-11-27 07:30:48 -0600

    Refine status designation logic

--- pqi-etl/proto/pqi_dataload_xlsx.py
+++ pqi-etl/proto/pqi_dataload_xlsx.py
@@ -30,7 +30,7 @@ def create_name_dataset(rows, columns):
     try:
       for columnName, columnIndex in columns:
         columnValue = row[columnIndex]
-        columnValue = columnValue if columnValue is not None else 'N/A'
+        columnValue = columnValue if columnValue is not None else ''
         rowTuple = (*rowTuple, columnValue,)
       lookupSet.add(rowTuple)
       lookupList.append(rowTuple)
@@ -74,7 +74,7 @@ def create_xlsx(file_name, sheetName, columns, dataRows):
       ws.append(row)
   ws.freeze_panes = 'A2'
   ws.auto_filter.ref = ws.dimensions
-  # ws["A1"].fill = PatternFill("solid", start_color="c9c9c9")
+  ws["A1"].fill = PatternFill("solid", start_color="c9c9c9")
 
   try:
     file_name_xlsx = file_name + '.xlsx'
@@ -94,28 +94,23 @@ def create_xlsx(file_name, sheetName, columns, dataRows):
 
 def create_champion_actions_xlsx(file_name, statusList, memberRows, nonMemberRows, championRows):
   createNewMemberColumnNames = ['Last Name', 'First Name', 'Email', 'Username']
-  upgradeMemberColumnNames = ['Last Name', 'First Name', 'Email', 'Website ID', 'Username']
+  upgradeMemberColumnNames = ['Last Name', 'First Name', 'Email', 'Username', 'Website ID', 'Champion']
   verifyColumnNames = ['Last Name', 'First Name', 'Email']
   
   wb = Workbook()
   ws = wb.active
   ws.title = 'Create'
   ws.append(createNewMemberColumnNames)
-  for name, status in statusList:
+  for keyValues, status in statusList:
     if status == 'non-member':
-      lastName, firstName = name
-      emailValue = 'N/A'
-      # Do a full 'table' scan for each individual 
-      for item in championRows:
-        if (item[0] == firstName) and (item[1] == lastName):
-          emailValue = item[2]
+      lastName, firstName, emailValue = keyValues
       if len(lastName) < 9:
         userName = lastName + firstName[0:1]
       else:
         userName = lastName[0:9] + firstName[0:1]
       userName = userName.lower()
-      row = name
-      row = (*row, emailValue, userName,)
+      row = keyValues
+      row = (*row, userName,)
       ws.append(row)
     else:
       continue
@@ -126,20 +121,19 @@ def create_champion_actions_xlsx(file_name, statusList, memberRows, nonMemberRow
   wb.create_sheet('Upgrade')
   ws = wb['Upgrade']
   ws.append(upgradeMemberColumnNames)
-  for name, status in statusList:
-    lastName, firstName = name
+  for keyValues, status in statusList:
+    lastName, firstName, emailValue = keyValues
     if status == 'member':
-      emailValue = 'N/A'
       for item in memberRows:
-        if (item[4] == firstName) and (item[3] == lastName):
+  # Column Y (zero based index = 24) contains the email value in the member export spreadsheet
+        if item[24] == emailValue:
           # Column L (Website ID (RO)) in the member export spreadsheet
           memberID = item[11] ;
           # Column W in the member export spreadsheet
           memberUsername = item[22] ;
-          # Column Y in the member export spreadsheet
-          memberEmailValue = item[24] ;
-      row = name
-      row = (*row, memberEmailValue, memberID, memberUsername, )
+          championFlag = 'Yes' ;
+      row = keyValues
+      row = (*row, memberUsername, memberID, championFlag)
       ws.append(row)
     else:
       continue
@@ -150,16 +144,14 @@ def create_champion_actions_xlsx(file_name, statusList, memberRows, nonMemberRow
   wb.create_sheet('Verify')
   ws = wb['Verify']
   ws.append(verifyColumnNames)
-  for name, status in statusList:
-    if status == 'neither':
-      lastName, firstName = name
-      emailValue = 'N/A'
+  for keyValues, status in statusList:
+    if status == 'verify':
+      lastName, firstName, emailValue = keyValues
         # Do a full 'table' scan for each individual 
-      for item in championRows:
-        if (item[0] == firstName) and (item[1] == lastName):
-          emailValue = item[2]
-      row = name
-      row = (*row, emailValue, )
+      # for item in championRows:
+      #   if (item[0] == firstName) and (item[1] == lastName):
+      #     emailValue = item[2]
+      row = keyValues
       ws.append(row)
     else:
       continue
@@ -186,12 +178,42 @@ def create_champion_actions_xlsx(file_name, statusList, memberRows, nonMemberRow
 def find_champion_status(championNames, memberNames, nonMemberNames):
   statusList = []
   for champion in championNames:
-    if champion in memberNames:
-      status = 'member'
-    elif champion in nonMemberNames:
-      status = 'non-member'
-    else:
-      status = 'neither'
+    status = 'verify'
+    lastNameChampion, firstNameChampion, emailChampion = champion
+    emailChampion = emailChampion.lower()
+    for keyValues in memberNames:
+      lastName, firstName, emailMember = keyValues
+      if emailChampion == emailMember.lower():
+        status = 'member'
+    for keyValues in nonMemberNames:
+      lastName, firstName, emailNonMember = keyValues
+      if emailChampion == emailNonMember.lower():
+        status = 'non-member'
+    if emailChampion is None or emailChampion == '':
+      status = 'verify'
+  # For blank champion email addresses see if can find in YM downloads using First & Last names
+    if status == 'verify':
+      bFound = False
+      for keyValues in memberNames:
+        lastName, firstName, emailMember = keyValues
+        if (firstNameChampion == firstName) and (lastNameChampion == lastName):
+          if emailChampion != emailMember:
+            print(f"{firstNameChampion} {lastNameChampion} status changed based on name match:")
+            print(f"   champion email = '{emailChampion}' - Member email = '{emailMember}'")
+          status = 'member'
+          bFound = True
+      for keyValues in nonMemberNames:
+        lastName, firstName, emailNonMember = keyValues
+        if (firstNameChampion == firstName) and (lastNameChampion == lastName):
+          if emailChampion != emailNonMember:
+            print(f"{firstNameChampion} {lastNameChampion} status changed based on name match:")
+            print(f"   champion email = '{emailChampion}' - Non-member email = '{emailMember}'")
+          status = 'non-member'
+          bFound = True
+      if bFound:
+        print(f"   Status changed from 'verify' to '{status}'.")
+
+  # Need champion key values in next stage of the process
     statusList.append((champion, status,))
 
   histogramCount = {}
@@ -201,6 +223,12 @@ def find_champion_status(championNames, memberNames, nonMemberNames):
     except KeyError:
         histogramCount[status] = 1
   print(f"\nChampion status histogram = {histogramCount}")
+  numChampionNames = len(championNames)
+  numStatus = len(statusList)
+  if numChampionNames == numStatus:
+    print(f"All {numChampionNames} champion records have been processed.")
+  else:
+    print(f"Error: {numChampionNames} champion records does not match {numStatus} status determinations.")
 
   return statusList
 
@@ -211,13 +239,15 @@ if __name__ == "__main__":
   memberRows = xlsx_reader("pqi-etl/data/20221125_members_export_excel_ymx.xlsx")
   # Exclude header row from row count
   print(f"Members (xlsx): {len(memberRows) - 1}")
-  columns = [('Last Name', 3,), ('First Name', 4)]
+  # Column Y (zero based index = 24) contains the email value in the member export spreadsheet
+  columns = [('Last Name', 3,), ('First Name', 4,), ('Email Address', 24,)]
   memberNames = create_name_dataset(memberRows, columns)
   print(f"Member unique names: {len(memberNames)}")
   create_xlsx('pqi-etl/data/member_unique_names', 'names', columns, memberNames)
 
   nonMemberRows = xlsx_reader("pqi-etl/data/20221125_non_members_export_excel_ymx.xlsx")
-  columns = [('Last Name', 2,), ('First Name', 3)]
+  # Column U (zero based index = 20) contains the email value in the non_member export spreadsheet
+  columns = [('Last Name', 2,), ('First Name', 3,), ('Email Address', 20,)]
   # Exclude header row from row count
   print(f"\nNon-Members (xlsx): {len(nonMemberRows) - 1}")
   nonMemberNames = create_name_dataset(nonMemberRows, columns)
@@ -225,8 +255,8 @@ if __name__ == "__main__":
   create_xlsx('pqi-etl/data/non_member_unique_names', 'names', columns, nonMemberNames)
 
   championRows = xlsx_reader("pqi-etl/data/Master List of all SPEAK UP Champion Attendees.xlsx")
-  columns = [('Last Name', 1,), ('First Name', 0)]
-  # columns = [('Last Name', 1,), ('First Name', 0), ('Email', 2)]
+  # Column C (zero based index = 2) contains the email value in the champion export spreadsheet
+  columns = [('Last Name', 1,), ('First Name', 0), ('Email', 2)]
   # Exclude header row from row count
   print(f"\nChampions (xlsx): {len(championRows) - 1}")
   championNames = create_name_dataset(championRows, columns)
